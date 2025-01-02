ebe267da32fb9c726cf917e8c988c76e5358e388
Rahultr45 <itsrahul.trtr@gmail.com>
2025-01-02 14:01:30 +0530

    emi calculator pre commit

---
+++ my-app/src/components/InterestRateList.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const InterestRateList: React.FC = () => {
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/interestRates')
+      .then((response) => response.json())
+      .then((data) => setRates(data));
+  }, []);
+
+  return (
+    <ul>
+      {rates.map((rate: { id: number; bankName: string; rate: number }) => (
+        <li key={rate.id}>
+          {rate.bankName}: {rate.rate}%
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default InterestRateList;
