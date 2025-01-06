70a07a45eff591bee6aabeca0d1f998812771ff3
hrithikt <hrithikt1999@gmail.com>
2025-01-06 19:20:36 +0530

    Revert "add config changes"
    This reverts commit 954b0b8c27280271cdfc1d659d93773ca6ec37b2.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/emiCalculator.tsx";
 import About from "./pages/aboutUs.tsx";
 import Blog from "./pages/blog.tsx";
