954b0b8c27280271cdfc1d659d93773ca6ec37b2
Rahultr45 <itsrahul.trtr@gmail.com>
2025-01-06 16:38:36 +0530

    add config changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/emiCalculator.tsx";
 import About from "./pages/aboutUs.tsx";
 import Blog from "./pages/blog.tsx";
